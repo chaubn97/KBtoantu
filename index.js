7d9fde867d416ca0b222f7cd730cb0b82e441a22
chaubn97 <lmnmc1928@gmail.com>
2019-09-24 10:23:49 +0200

    git24.9.2019

---
+++ index.js
@@ -0,0 +1,23 @@
+var i= 10 ;
+var f = 20.5 ;
+var b = true ;
+var[] s = "Ha noi"
+
+document.write('i = ' + i)
+document.write('<br/>')
+document.write('f = ' + f)
+document.write('<br/>')
+document.write('b = ' + b)
+document.write('<br/>')
+document.write('s = ' + s)
+
+
+let width = 20
+let height = 10
+let area = width * height
+document.write('Area = ' + area)
+
+var a = prompt();
+var b = prompt();
+var c= (a%b===0 );
+alert(c);
